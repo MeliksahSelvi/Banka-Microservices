9ffea7a2955ea7e222f9f34724e5f4fc0cffd3c7
mselvi <mselvi@uni-yaz.com>
2023-09-15 23:51:17 +0300

    -added test spring boot applications

---
+++ identity-service/src/test/java/com/melik/identityservice/IdentityServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.melik.identityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author mselvi
+ * @Created 11.09.2023
+ */
+
+@SpringBootTest
+public class IdentityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ logging-service/src/test/java/com/melik/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.melik.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author mselvi
+ * @Created 08.09.2023
+ */
+
+
+@SpringBootTest
+public class LoggingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
